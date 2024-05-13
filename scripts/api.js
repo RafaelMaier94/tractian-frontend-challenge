b2460752ac8f187ec0bb8dd2af86086f305ac872
rafael.maier <rafael.maier@eemovel.com>
2024-05-13 09:53:22 -0300

    feat: get locations from api

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,17 @@
 export const getCompanies = async () => {
     try{
-
         const response = await fetch("https://fake-api.tractian.com/companies");
         return response.json();
     }catch(e){
-        alert("Erro ao buscar dados na API")
+        alert("Erro ao buscar companhias")
+    }
+}
+
+export const getCompanyLocations = async (companyId) => {
+    try {
+        const response = await fetch(`https://fake-api.tractian.com/companies/${companyId}/locations`)        
+        return response.json();
+    } catch (e) {
+        alert("Erro ao buscar localizações")
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-import { getCompanies } from "./api.js";
-import { companiesSVG } from "./svgMappers.js";
+import { getCompanies, getCompanyLocations } from "./api.js";
+import { arrowSVG, companiesSVG, locationsSVG } from "./svgMappers.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     addListeners()
@@ -20,18 +20,19 @@ function createElementFromHTML(htmlString) {
   
     // Change this to div.childNodes to support multiple top-level nodes.
     return div.firstChild;
-  }
+}
 
-async function loadCompanies () {
+async function loadCompanies() {
     const companies = await getCompanies();
     const companiesContainer = document.getElementById("companies-container");
-    companies.forEach(({name}) => {
+    companies.forEach(({name, id}) => {
 
         const button = document.createElement("button")
         button.className = "unit"
-        button.id = name
+        button.id = id
 
         const text = document.createElement("span")
+        text.id = `${id}-name`
         text.innerText = `${name} Unit`
 
         button.appendChild(createElementFromHTML(companiesSVG))
@@ -45,13 +46,15 @@ async function loadCompanies () {
 
 }
 
-getCompanies();
+loadLocations()
+
+async function loadLocations() {
+    const locations = await getCompanyLocations("662fd0ee639069143a8fc387");
+    console.log(locations)
+}
+
 
 function addListeners (){
-    const sidebar = document.getElementById('sidebar--closed');
-    if(sidebar){
-        sidebar.addEventListener("click", handleSidebarOpen)
-    }
     const innerWidth = window.innerWidth
     if (innerWidth <= 768){
         console.log("inside")
@@ -62,7 +65,27 @@ function addListeners (){
         Array.from(sections).forEach(section => {addEventListener('dragstart', handleTouchStart)})
         Array.from(sections).forEach(section => {addEventListener('dragend', handleTouchEnd)})
     }
+    const searchField = document.getElementById("search-text");
+    searchField.addEventListener('input', debounce(handleSearch, 500))
+    const locationsButtons = document.getElementsByClassName("location");
+    Array.from(locationsButtons).forEach(button => button.addEventListener("click", handleLocationsArrow))
 
+}
+
+
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+}
+
+function handleSearch(event) {
+    console.log(event)
+    console.log(event.target.value)
 }
 
 let startInteractionPosition;
@@ -108,14 +131,13 @@ const handleTouchEnd = (event) => {
     }
 
 }
-
-function handleClickUnitButton (event) {
-    const { currentTarget: { id } } = event
-    const button = document.getElementById(event.currentTarget?.id)
-    const isActive = (button) => button.classList.contains("active")
-    const unitButtons = document.getElementsByClassName("unit")
+async function handleClickUnitButton (event) {
+    const { currentTarget: { id } } = event;
+    const button = document.getElementById(event.currentTarget?.id);
+    const isActive = (button) => button.classList.contains("active");
+    const unitButtons = document.getElementsByClassName("unit");
     const activeUnit = document.getElementById('active-unit-name');
-    const unitText = `${id} Unit`
+    const unitText = document.getElementById(`${id}-name`)
     // activeUnit.innerHTML = 
     Array.from(unitButtons).forEach(button => {
         if (isActive(button) && button.id !== id){
@@ -127,10 +149,50 @@ function handleClickUnitButton (event) {
         return;
     }
     if(activeUnit){
-        activeUnit.innerText = ` / ${unitText}`
+        activeUnit.innerText = ` / ${unitText.innerText}`
     }
-
     button.classList.add("active")
+    const locations = await getCompanyLocations(id)
+    renderLocations(locations)
+}
+
+const handleLocationsArrow = (event) => {
+    console.log(event.target)
+    const arrow = document.getElementById(event.target.id);
+    if(arrow.classList.contains("open")){
+        arrow.classList.remove("open")
+        arrow.classList.add("closed")
+    } else {
+        arrow.classList.remove("closed")
+        arrow.classList.add("open")
+    }
+}
+
+const renderLocations = (locations) => {
+    const parent = document.getElementsByClassName("assets-container")[0];
+    console.log(parent)
+    locations.forEach((location) => {
+        const button = document.createElement("button");
+        button.classList.add("location")
+        button.classList.add("closed")
+        button.id = location.id
+        
+        const arrow = document.createElement("span");
+        arrow.classList.add("location-arrow");
+        arrow.appendChild(createElementFromHTML(arrowSVG));
+
+        const textSpan = document.createElement("span");
+        textSpan.innerText = location.name;
+
+        button.addEventListener("click", handleLocationsArrow)
+
+        button.appendChild(arrow)
+        button.appendChild(createElementFromHTML(locationsSVG))
+        button.appendChild(textSpan)
+        console.log(parent)
+
+        parent.appendChild(button)
+    })
 }
 
 const handleSidebarOpen = (event) => {
